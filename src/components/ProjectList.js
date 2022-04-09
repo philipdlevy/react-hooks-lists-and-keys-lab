ac5c5e82317e03a8a938950dde164a8eccdd409f
Philip Levy <philipdlevy@gmail.com>
2022-04-09 11:27:44 -0600

    commit tree 5481a6a19dbbe0c81ab986aaeb3514f90d733354

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,32 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  console.log("projects", projects);
+
+  const projectArray = projects.map((project) => {
+    return <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}></ProjectItem>
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectArray}</div>
     </div>
   );
 }
 
 export default ProjectList;
+
+
+
+
+
+// function ProjectList({ projects }) {
+  //   console.log("projects", projects);
+  //   return (
+  //     <div id="projects">
+  //       <h2>My Projects</h2>
+//       <div id="project-list">{/* render ProjectItem components here */}</div>
+//     </div>
+//   );
+// }
